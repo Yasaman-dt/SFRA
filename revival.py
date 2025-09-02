b8b37500e0df5c5861cdec3905d1d3766c3037ef
Yasaman-dt <yasaman.dehghani74@gmail.com>
2025-09-02 19:14:04 -0400

    update

--- revival.py
+++ revival.py
@@ -28,7 +28,8 @@ import argparse
 parser = argparse.ArgumentParser("Class unlearning revival")
 parser.add_argument('--method', type=str, default='original',
                     choices=['original','retrained','random_label','finetune','gradient_ascent',
-                             'boundary_shrink','boundary_expand','l2ul_adv','fisher','wood_fisher','delete'])
+                             'boundary_shrink','boundary_expand','l2ul_adv','fisher','wood_fisher','delete',
+                             'bad_teacher', 'salun'])
 # accept both --model and --model_name, store in model_name
 parser.add_argument('--model', '--model_name', dest='model_name', type=str, default='resnet18')
 parser.add_argument('--dataset', type=str, required=True)
